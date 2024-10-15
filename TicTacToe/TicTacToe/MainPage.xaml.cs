e6f1d3896f577075e5b59c256955326066eafa41
Kacper <webbykacper@gmail.com>
2024-10-15 19:35:38 +0200

    Added new project - tictactoe

---
+++ TicTacToe/TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/VievModels/TicTacToeVievModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.VievModels
+{
+    partial class TicTacToeVievModel : BindableObject
+    {
+		string mark = "X";
+		string[,] board = new string[3, 3];
+		private Command<Button> markCellCommand;
+		public Command<Button> MarkCellCommand
+		{
+			get { return markCellCommand; }
+			set { markCellCommand = value; }
+		}
+
+		public TicTacToeVievModel()
+		{
+			MarkCellCommand = new Command<Button>((button) =>
+			{
+                if (button.Text == null)
+                {
+					switchMark(button);
+                }
+            });
+		}
+
+		private void switchMark(Button button)
+		{
+			button.Text = mark;
+
+            int row = Grid.GetRow(button);
+            int column = Grid.GetColumn(button);
+
+            board[column, row] = mark;
+            mark = mark == "X" ? mark = "O" : mark = "X";
+        }
+	}
+}
